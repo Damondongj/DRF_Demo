c0f75078ea0379902aa92c26e5b66378bb94614e
Damondongj <18226669129@163.com>
2023-04-18 08:55:29 +0800

    spider

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
 import requests
+from lxml import etree
 from retrying import retry
 from bs4 import BeautifulSoup
 from fake_headers import Headers
 
-BASE_URL = "http://www.51hei.com/"
+BASE_URL = "http://www.51hei.com"
 
 
 @retry(stop_max_attempt_number=3, retry_on_result=lambda x: x is None, wait_fixed=2000)
@@ -34,26 +35,32 @@ def parse_outer():
             href = BASE_URL + child["href"]
             if title.__contains__("PCB") or title.__contains__("pcb") or title.__contains__(
                     "Pcb") or title.__contains__("原理图"):
+                # if href != "http://www.51hei.com/bbs/dpj-216384-1.html":
                 inner[title] = href
     return inner
-
+ 
 
 def parser_inner(title_url):
     for title, url in title_url.items():
         html = fetch(url)
-        soup = BeautifulSoup(html, "html.parser")
-        postlist_div = soup.find('div', {'id': 'postlist'})
-        first_div = postlist_div.find('div', recursive=False)
-        attnm_p = first_div.find("p", {"class": "attnm"})
-        href = attnm_p.find("a")["href"]
-        title_url[title] = href
+        html = etree.HTML(html)
+        print(url)
+        try:
+            postlist_div = html.xpath('//*[@id="postlist"]')[0]
+            first_div = postlist_div.xpath('./div')[0]
+            attnm_div = first_div.find('.//p[@class="attnm"]')
+            print(attnm_div)
+            print(first_div.get("id"))
+        except Exception:
+            print("HTML error!!!")
+            continue
     return title_url
 
 
 def main():
     title_url = parse_outer()
     title_url = parser_inner(title_url)
-    print(title_url)
+    # print(title_url)
 
 
 if __name__ == '__main__':
