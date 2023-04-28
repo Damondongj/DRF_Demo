78c6d56e8b378b0205fcd5f830994c460ab6d2df
Damondongj <18226669129@163.com>
2023-04-28 14:30:54 +0800

    changes

--- generate.py
+++ generate.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import plotly.express as px
-from PIL import Image as PILImage
 import plotly.graph_objects as go
 
 from reportlab.lib import colors
@@ -13,6 +12,8 @@ from reportlab.lib.styles import ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import SimpleDocTemplate, Table, Image, Paragraph, Spacer, PageBreak, TableStyle
+
+TEMP_PATH = "/Users/damon/projects/web/DRF_Demo/media"
 
 
 class PlotImage(object):
@@ -56,7 +57,7 @@ class PlotImage(object):
         fig.add_annotation(x=0.5, y=0.2, text="分数", font=dict(family='Arial', size=32, color='#E83F2F'),
                            showarrow=False)
 
-        fig.write_image('plotly_2.png')
+        fig.write_image(TEMP_PATH + 'plotly_2.png')
 
     def plotly_part3(self):
         plot_data = [{'question_type': '反射天线', 'number_of_selected': 5},
@@ -81,7 +82,7 @@ class PlotImage(object):
         fig.update_traces(textposition='outside', textinfo='percent+label', textfont_size=12,
                           insidetextorientation='radial')
 
-        fig.write_image("plotly_3.png")
+        fig.write_image(TEMP_PATH + "plotly_3.png")
 
     def plotly_part4(self):
         data = {'Method': ['Method 1', 'Method 2', 'Method 3', 'Method 4', 'Method 5', 'Method 6'],
@@ -93,9 +94,8 @@ class PlotImage(object):
         fig = px.bar(df, x='Method', y='Accuracy', text=df['Accuracy'].apply(lambda x: f'{x:.2%}'), color=colors)
         fig.update_traces(textposition='auto')
         fig.update_layout(
-            width=500, height=380, title='能力图谱分析', xaxis_title='', yaxis_title='准确率', showlegend=False
-        )
-        fig.write_image('plotly_4.png')
+            width=500, height=380, title='能力图谱分析', xaxis_title='', yaxis_title='准确率', showlegend=False)
+        fig.write_image(TEMP_PATH + 'plotly_4.png')
 
     def plotly_part5(self):
         datas = [
@@ -135,25 +135,25 @@ class PlotImage(object):
         ]
 
         titles = list(datas[0].keys())
-
         values = [[data[title] for data in datas] for title in titles]
 
         table = go.Table(
             header=dict(values=titles),
-
             cells=dict(values=values)
         )
 
         fig = go.Figure(data=[table])
         fig.update_layout(
-            # width=500, height=380
             margin=dict(t=20, b=0),
         )
 
-        fig.write_image('plotly_5.png', format='png')
+        fig.write_image(TEMP_PATH + 'plotly_5.png', format='png')
 
 
 def generate():
+    """
+        generate pdf
+    """
     plot = PlotImage()
     plot.plot()
 
@@ -168,11 +168,12 @@ def generate():
     pdfmetrics.registerFont(TTFont('SimSun', 'simsun.ttc'))
     pdfmetrics.registerFont(TTFont('Microsoft YaHei', 'msyh.ttc'))
 
-    if os.path.exists("report.pdf"):
-        os.remove("report.pdf")
+    file_path = TEMP_PATH + "report.pdf"
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
     # 创建PDF文档
-    doc = SimpleDocTemplate("report.pdf", pagesize=letter,
+    doc = SimpleDocTemplate(file_path, pagesize=letter,
                             rightMargin=MARGIN, leftMargin=MARGIN,
                             topMargin=MARGIN, bottomMargin=MARGIN)
     elements = []
@@ -193,11 +194,8 @@ def generate():
     para_style = ParagraphStyle(name='para_style', fontName=TABLE_FONT, fontSize=10, leading=16)
     data = [
         [Paragraph("被测EDA名称：", label_style), Paragraph("星火技术", para_style)],
-        [Paragraph("被测EDA简介：", label_style), Paragraph(
-            "这是CST",
-            para_style)]
+        [Paragraph("被测EDA简介：", label_style), Paragraph("这是CST", para_style)]
     ]
-
 
     table = Table(data, colWidths=[90, 320])
     table_style = TableStyle([
@@ -219,20 +217,14 @@ def generate():
     section2_style = ParagraphStyle(name='section_style', fontName=TITLE_FONT, fontSize=FONTSIZE_SM)
     elements.append(Paragraph("2、考试成绩", section2_style))
     elements.append(Spacer(1, 0.1 * inch))
-    img = PILImage.open("plotly_2.png")
-    width, height = img.size
-    aspect = height / float(width)
-    elements.append(Image("plotly_2.png", width=4 * inch, height=3 * inch))
-    # elements.append(Spacer(1, 0.5 * inch))
+    elements.append(Image(TEMP_PATH + "plotly_2.png", width=4 * inch, height=3 * inch))
 
     # 3、试卷内容1
     section3_style = ParagraphStyle(name='section_style', fontName=TITLE_FONT, fontSize=FONTSIZE_SM)
     elements.append(Paragraph("3、试卷内容", section3_style))
     elements.append(Spacer(1, 0.1 * inch))
-    img = PILImage.open("plotly_3.png")
-    width, height = img.size
-    aspect = height / float(width)
-    elements.append(Image("plotly_3.png", width=4.4 * inch, height=3.3 * inch))
+    elements.append(
+        Image(TEMP_PATH + "plotly_3.png", width=4.4 * inch, height=3.3 * inch))
 
     elements.append(PageBreak())
 
@@ -240,23 +232,18 @@ def generate():
     section4_style = ParagraphStyle(name='section_style', fontName=TITLE_FONT, fontSize=FONTSIZE_SM)
     elements.append(Paragraph("4、试卷内容", section4_style))
     elements.append(Spacer(1, 0.1 * inch))
-    img = PILImage.open("plotly_4.png")
-    width, height = img.size
-    aspect = height / float(width)
-    elements.append(Image("plotly_4.png", width=6 * inch, height=4.5 * inch))
+    elements.append(Image(TEMP_PATH + "plotly_4.png", width=6 * inch, height=4.5 * inch))
     elements.append(Spacer(1, 0.3 * inch))
 
     # 添加答题详情
     section5_style = ParagraphStyle(name='section_style', fontName=TITLE_FONT, fontSize=FONTSIZE_SM)
     elements.append(Paragraph("5、答题详情", section5_style))
     elements.append(Spacer(1, 0.1 * inch))
-    img = PILImage.open("plotly_5.png")
-    width, height = img.size
-    aspect = height / float(width)
-    elements.append(Image("plotly_5.png", width=6 * inch, height=4.5 * inch))
+    elements.append(Image(TEMP_PATH + "plotly_5.png", width=6 * inch, height=4.5 * inch))
 
-    # 将所有元素添加到文档中并生成PDF文件
     doc.build(elements)
+
+    return file_path
 
 
 if __name__ == '__main__':
